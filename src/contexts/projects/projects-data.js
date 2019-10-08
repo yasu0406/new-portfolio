4c216a224d2d04439556a576010b096ac403d063
yasuhiro <yasucom4646@gmail.com>
2019-10-07 17:28:58 -0700

    Added new personal project of pic and data

--- src/contexts/projects/projects-data.js
+++ src/contexts/projects/projects-data.js
@@ -1,6 +1,23 @@
 const PROJECTS_DATA = [
     {
         'id': '01',
+        'title': 'FIND',
+        'description': 'Designing and planning personal project that is housing service for people. utilizing ReactJS, ContextAPI, Javascript, Mongodb and Firebase',
+        'thumbnail': '/images/thumbnail-05.jpg',
+        'imgUrl' : '/images/mv-05.png',
+        'projectImg01' : '/images/projects05-img-01.jpg',
+        'projectImg02' : '/images/projects05-img-02.jpg',
+        'projectImg03' : '/images/projects05-img-03.jpg',
+        'bgColor': '#DC3545',
+        'url': 'find',
+        'viewLink': '',
+        'develop': 'Web Development',
+        'project': 'Personal Project',
+        'useProgram':'HTML5, CSS, ReactJS, ContextAPI, Javascript, Mongodb, Firebase',
+        'date': 'October 1, 2019'
+    },
+    {
+        'id': '02',
         'title': 'JK EXCELLENCE',
         'description': 'Designing and planning project goals. Managing task delegation within team. Meeting with clients to discuss about client needs. Designing and creating an interface for JK Excellence to market tutoring service for prospective students utilizing ReactJS, Redux, Javascript, WordPress and Semantic UI',
         'thumbnail': '/images/thumbnail-01.jpg',
@@ -11,10 +28,13 @@ const PROJECTS_DATA = [
         'bgColor': '#44C5C2',
         'url': 'jk-excellence',
         'viewLink': 'https://jkdev.yasuhiro-k.com/',
+        'develop': 'Web Development',
+        'project': 'Freelance',
+        'useProgram':'HTML5, CSS, ReactJS, Redux, Javascript, WordPress',
         'date': 'June 1, 2019'
     },
     {
-        'id': '02',
+        'id': '03',
         'title': 'METAL REVISION',
         'description': 'Developed a company website for factory, using HTML5, CSS, BootStrap, jQuery, javascript and WordPress Communicated with designers and project managers for project milestones Tested functionalities of final product',
         'thumbnail': '/images/thumbnail-02.jpg',
@@ -25,10 +45,13 @@ const PROJECTS_DATA = [
         'bgColor': '#1F2E96',
         'url': 'metal-revision',
         'viewLink': 'http://www.metal-rev.co.jp/',
+        'develop': 'Web Development',
+        'project': 'Freelance',
+        'useProgram':'HTML5, CSS, jQuery, WordPress',
         'date': 'February 1, 2019'
     },
     {
-        'id': '03',
+        'id': '04',
         'title': 'SOCCER MEET',
         'description': 'Designing and planning project goals Designing and creating an interface for soccer teams utilizing ReactJS, Firebase, Javascript. Using XD to create design.',
         'thumbnail': '/images/thumbnail-03.jpg',
@@ -39,10 +62,13 @@ const PROJECTS_DATA = [
         'bgColor': '#00807C',
         'url': 'soccer-meet',
         'viewLink': '',
+        'develop': 'Mobile Development',
+        'project': 'Personal Project',
+        'useProgram':'React Native, Firebase',
         'date': 'July 1, 2019'
     },
     {
-        'id': '04',
+        'id': '05',
         'title': 'HST CO. LTD',
         'description': 'Developed a company website for car dealer, using HTML5, CSS, BootStrap, jQuery, javascript and WordPress. Meeting with clients to discuss about client needs. Designing and creating an interface for HST CO. LTD to market car dealer.',
         'thumbnail': '/images/thumbnail-04.jpg',
@@ -50,9 +76,12 @@ const PROJECTS_DATA = [
         'projectImg01' : '/images/projects04-img-01.jpg',
         'projectImg02' : '/images/projects04-img-02.jpg',
         'projectImg03' : '/images/projects04-img-03.jpg',
-        'bgColor': '#DB2D2E',
+        'bgColor': '#000000',
         'url': 'hst-co',
         'viewLink': '',
+        'develop': 'Web Development',
+        'project': 'Personal Project',
+        'useProgram':'HTML5, CSS, jQuery, WordPress',
         'date': 'July 1, 2019'
     },
 ]
